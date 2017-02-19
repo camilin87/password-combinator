86fc06435408f6d875f443b1af26d51a2e6d56e7
Camilo Sanchez <camilin87@gmail.com>
2017-02-18 21:51:37 -0500

    minor test refactor

--- spec/combinator_spec.rb
+++ spec/combinator_spec.rb
@@ -26,31 +26,25 @@ describe 'Combinator' do
         ])
     end
 
-    it 'combines O to zeros' do
+    it 'circle like things' do
         expect(Combinator.generate('o')).to match_array([
             'o', 'O', '0'
         ])
-    end
 
-    it 'combines zeros to Os' do
         expect(Combinator.generate('0')).to match_array([
             '0', 'o', 'O'
         ])
     end
 
-    it 'combines ones to is and ls' do
+    it 'stick like things' do
         expect(Combinator.generate('1')).to match_array([
             '1', 'i', 'I', 'l', 'L'
         ])
-    end
 
-    it 'combines is to 1s and ls' do
         expect(Combinator.generate('i')).to match_array([
             'i', 'I', '1', 'l', 'L'
         ])
-    end
 
-    it 'combines ls to 1s and is' do
         expect(Combinator.generate('l')).to match_array([
             'l', 'L', '1', 'i', 'I'
         ])
